c09a391317b67ea33c794367cb4c0cde266df124
sagor20202021 <sagorahammed1562@gamil.com>
2022-03-29 01:34:45 +0600

    route start rpo

---
+++ src/Components/About/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <h1>This is about us</h1>
+    </div>
+  );
+};
+
+export default About;
+<h1>This is about us</h1>;
---
+++ src/Components/Freinds/Freinds.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Freinds = () => {
+  return (
+    <div>
+      <h2>Hello freinds</h2>
+      <p>
+        <small>parle kisu taka dar dis</small>
+      </p>
+    </div>
+  );
+};
+
+export default Freinds;
